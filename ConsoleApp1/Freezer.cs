bce1ad6f8f720ba50dc9bf0f08c06a87665cb65d
Vlad805 <zhomyruk_ak18@nuwm.edu.ua>
2020-09-20 13:23:13 +0300

    first commit

---
+++ ConsoleApp1/Freezer.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    partial class Freezer
+    {
+        public bool GetisOpen()
+        {
+            return isOpen;
+        }
+
+        public void SetIsOpen(bool is_op)
+        {
+            this.isOpen = is_op;
+        }
+
+        public string Get_Brand()
+        {
+            return brand;
+        }
+
+        public void Set_Brand(string brand)
+        {
+            this.brand = brand;
+        }
+
+        public void Show()
+        {
+            Console.WriteLine("Temperature: " + temperature);
+            Console.WriteLine("Volume: " + volume);
+            Console.WriteLine("Color: " + color);
+            Console.WriteLine("Brand: " + brand);
+            Console.WriteLine("Is open: " + isOpen);
+            Console.WriteLine("Height: " + height);
+            Console.WriteLine("Id freezer" + id_Freezer);
+            Console.WriteLine("Count: " + count);
+        }
+
+        public void Open_freezer()
+        {
+            isOpen = true;
+            Console.WriteLine("Freezer is open!");
+        }
+
+        public void Close_freezer()
+        {
+            isOpen = false;
+            Console.WriteLine("Freezer is close!");
+        }
+
+        public void Сhange_volum(ref double volume)
+        {
+            volume = this.volume;
+        }
+
+        public override string ToString()
+        {
+            return $"brand: {brand}\n" +
+                   $"color: {color}\n" +
+                   $"volume: {volume}\n" +
+                   $"temperature: {temperature}\n" +
+                   $"Height: {height}\n" +
+                   $"id_Freezer: {id_Freezer}\n" +
+                   $"Is open: {isOpen}\n" +
+                   $"Count: {count}";
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+
+    public partial class Freezer
+    {
+        private int temperature { get; set; }
+        private double volume { get; set; }
+        private string color { get; set; }
+        private bool isOpen;
+        private string brand;
+        private float height { get; set; }
+
+        private static int count;
+        public static int Count
+        {
+            get { return count; }
+        }
+
+        public static int id_Freezer { get; }
+        static Freezer()
+        {
+            count = 0;
+            id_Freezer = 123456;
+        }
+
+        public Freezer()
+        {
+            temperature = 0;
+            volume = 0;
+            color = "no color";
+            brand = "no brand";
+            isOpen = false;
+            height = 0;
+            count++;
+        }
+
+        public Freezer(int temperature, double volume, string color, string brand, bool isOpen, float height)
+        {
+            if (temperature < 0)
+                this.temperature = temperature;
+            else
+                this.temperature = 0;
+
+            if (volume >= 0)
+                this.volume = volume;
+            else
+                this.volume = 0;
+
+            this.color = color;
+
+            this.brand = brand;
+
+            this.isOpen = isOpen;
+
+            if (height > 0)
+                this.height = height;
+            else
+                height = 0;
+            count++;
+        }
+
+        public Freezer(int temperature, double volume, string color, string brand, float height) : this(temperature, volume, color, brand, false, height) { }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Freezer[] freezers = new Freezer[5];
+            for (int i = 0; i < 5; i++)
+            {
+                freezers[i] = new Freezer();
+                freezers[i].Show();
+                //Console.WriteLine(freezers[i].ToString());
+                Console.WriteLine();
+            }
+        }
+    }
+}
